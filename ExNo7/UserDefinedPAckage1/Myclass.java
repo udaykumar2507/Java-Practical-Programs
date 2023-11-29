25c69ff04c7216b2d7138a219d6e724a6f9cc352
Uday Kumar Maurya <122080383+udaykumar2507@users.noreply.github.com>
2023-11-29 23:49:41 +0530

    Add files via upload

---
+++ ExNo7/UserDefinedPAckage1/Myclass.java
@@ -0,0 +1,8 @@
+package ExNo7.UserDefinedPAckage1;
+
+public class Myclass {
+    public void displayMessage(){
+        System.out.println("Hello Brother");
+    }
+    
+}
---
+++ ExNo7/UserDefinedPackage2/Main.java
@@ -0,0 +1,11 @@
+package ExNo7.UserDefinedPackage2;
+import ExNo7.UserDefinedPAckage1.Myclass;
+public class Main {
+    public static void main(String[] args) {
+        Myclass mine=new Myclass();
+        mine.displayMessage();
+        
+    }
+
+    
+}
